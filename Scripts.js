cc0019c9ee6b72b85042d959dd18db773494a8e8
Palex85 <pauloajn1@hotmail.com>
2024-05-13 12:24:40 -0300

    first commit

---
+++ Scripts.js
@@ -0,0 +1,21 @@
+const list = document.querySelector('ul')
+const buttonShowAll = document.querySelector('.show-all')
+let myLi = ``
+
+
+function showAll() {
+    menuOptions.forEach((product) => {
+        myLi += `
+    <li >
+        <img src=${product.src}>
+        <p>${product.name}</p>
+        <p class="item-price">${product.price}</p>
+    </li>
+    
+    `
+    })
+    list.innerHTML = myLi
+}
+
+buttonShowAll.addEventListener('click', showAll)
+
